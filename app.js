76700be362f8e75ebd6c16832aa863b798fdf112
junneFYe <12868451+junnefye@user.noreply.gitee.com>
2026-03-29 17:37:06 +0800

    3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿const API_BASE_URL = 'http://localhost:8080/api';
+﻿const API_BASE_URL = 'http://101.201.235.13:8080/api';
 
 function formatYearMonthLocal(date) {
     const year = date.getFullYear();
